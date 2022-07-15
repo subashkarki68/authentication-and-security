2e0ea67711515d1c92d6e27241d7ab3693342c0c
subash karki <subashkarki68@gmail.com>
2022-07-15 16:36:10 +0545

    add support for heroku

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //Connecting to monfgoDB
 const URI = process.env.MONGOOSE_URI;
